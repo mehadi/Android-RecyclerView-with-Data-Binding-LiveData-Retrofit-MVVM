f0f5205c9ca307302fc1105350cf96e111f69ec2
mehadi <bdmehadih@gmail.com>
2024-03-22 13:06:58 +0600

    fixes

--- app/src/main/java/me/mehadih/retrofitlivedatamvvmrecyclerviewdatabinding/MainActivity.java
+++ app/src/main/java/me/mehadih/retrofitlivedatamvvmrecyclerviewdatabinding/MainActivity.java
@@ -6,7 +6,10 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
